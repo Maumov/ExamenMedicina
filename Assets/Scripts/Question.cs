1b5b2299ec71bddcbfc5d6cd058925c8954d641e
Mauricio <mau.movilla@gmail.com>
2019-06-18 02:24:52 -0500

    Manejo de Gdrive, y modelos de datos

---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question {
+    public string parrafo;
+    public string video;
+    public List<string> preguntas;
+    public List<string> respuestas;
+}
+
+
+
